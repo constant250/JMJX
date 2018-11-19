cf25484896474fd1ff593cd3396998135eacb631
Ian Rey <dlancermarco@gmail.com>
2018-11-19 11:14:37 +0800

    modified logout button

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -30,7 +30,12 @@
                   <a class="dropdown-item" href="#">My Profile</a>
                   <a class="dropdown-item" href="#">Settings</a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Logout</a>
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">Logout</a>
+                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      {{ csrf_field() }}
+                  </form>
                 </div>
               </li>
             </ul>
