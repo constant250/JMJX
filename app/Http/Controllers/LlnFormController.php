540914bcca8122a5e3d1e7a8438e32b8b48692c9
Jayr Mendoza <jayrmendoza1021@gmail.com>
2018-11-20 08:04:26 +0800

    Update LlnFormController.php

--- app/Http/Controllers/LlnFormController.php
+++ app/Http/Controllers/LlnFormController.php
@@ -14,7 +14,7 @@ class LlnFormController extends Controller
     public function index()
     {
         //
-        return view('lln-form,index');
+        return view('lln-form.index');
     }
 
     /**
