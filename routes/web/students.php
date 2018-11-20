b50031fb96e77ca4460d371e30631f9fd64a7605
constant250 <constant.claro@gmail.com>
2018-11-20 08:27:14 +0800

    updates in routes/controllers for views

--- routes/web/students.php
+++ routes/web/students.php
@@ -42,7 +42,7 @@ Route::prefix('student')->group( function() {
 	route::resource('refund-request','RefundRequestController');
 
 	// lln
-	route::resource('lln-from','LlnFormController');
+	route::resource('lln-form','LlnFormController');
 } );
 
 
